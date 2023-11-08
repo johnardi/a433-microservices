f55ed00c326196a1d22bae165fb1a7f554244d03
johnardi <yohanesardiandk@gmail.com>
2023-11-08 22:40:27 +0700

    proyek-pertama

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,11 +6,11 @@ docker build -t item-app:v1 .
 # List local Docker images
 docker images
 
-# Log in to GitHub Packages
+# Log in to Docker Hub
 cat ../dockerpashub.txt | docker login -u ardi0104 --password-stdin
 
-# Rename the image to match GitHub Packages format
+# Rename the image to match Docker Hub format
 docker tag item-app:v1 ardi0104/item-app:v1
 
-# Push the image to GitHub Packages
+# Push the image to Docker Hub Packages
 docker push ardi0104/item-app:v1
