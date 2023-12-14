ba77ad5f79c398aaac433ea7fc4b7e3ade7d589d
Yohanes Ardi <64395540+johnardi@users.noreply.github.com>
2023-12-14 23:53:25 +0700

    Add files via upload

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#build image docker dengan nama karsajobs-ui & tag latest
+docker build -t ghcr.io/johnardi/karsajobs:latest .
+echo "\n"
+#login ke GitHub menggunakan token (export CR_PAT=YOUR_TOKEN)
+echo $CR_PAT | docker login ghcr.io -u johnardi --password-stdin
+echo "\n"
+#push the image to GitHub Packages
+docker push ghcr.io/johnardi/karsajobs:latest
