88be3df5b491aba4261483cfa8361eb372ea332a
Yohanes Ardi <64395540+johnardi@users.noreply.github.com>
2023-12-19 23:19:25 +0700

    Add files via upload

---
+++ build_push_image_order_service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build image dengan tag v1
+docker build -t ghcr.io/johnardi/order-service:v1 .
+
+# Melihat daftar image Docker yang ada
+docker images
+
+# Menggunakan environment variable $CR_PAT untuk login ke GitHub
+echo $CR_PAT | docker login ghcr.io -u johnardi --password-stdin
+
+# Mengunggah image dengan tag v1
+docker push ghcr.io/johnardi/order-service:v1
