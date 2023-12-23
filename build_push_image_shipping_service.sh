b9bb940ce51333ae64db76f3e8d465e3db8e8564
Yohanes Ardi <64395540+johnardi@users.noreply.github.com>
2023-12-23 20:48:14 +0700

    Update build_push_image_shipping_service.sh

--- build_push_image_shipping_service.sh
+++ build_push_image_shipping_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build image dengan tag v1
-docker build -t ghcr.io/johnardi/shipping-service:v1 .
+docker build -t ghcr.io/johnardi/shipping-service:latest .
 
 # Melihat daftar image Docker yang ada
 docker images
@@ -9,5 +9,5 @@ docker images
 # Menggunakan environment variable $CR_PAT untuk login ke GitHub
 echo $CR_PAT | docker login ghcr.io -u johnardi --password-stdin
 
-# Mengunggah image dengan tag v1
-docker push ghcr.io/johnardi/shipping-service:v1
+# Mengunggah image dengan tag latest
+docker push ghcr.io/johnardi/shipping-service:latest
